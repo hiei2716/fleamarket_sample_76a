660809ff844af9d884437e7392f526b78f4f1ed7
Yu-41 <hirosi41@gmail.com>
2020-06-05 18:39:22 +0900

    ユーザーマイページ及びログアウト・クレジットカード登録ページのマークアップ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'items#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  get "logout" => "users#logout"
+  resources :credit_cards
+  get "new" => "credit_cards#new"
 end
