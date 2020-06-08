5f2d4f33366454d7896846e46031bd00b8e304fe
RyunosukeHorihata <ryunosuke.horihata@gmail.com>
2020-06-08 16:49:56 +0900

    chage color and routs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'items#index'
-  get '/purchase/index', to: 'purchase#index'
+  get 'purchase/index', to: 'purchase#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
