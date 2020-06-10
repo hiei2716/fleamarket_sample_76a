dee4391f9ff17acee3721b5ac0d50e94a80cedc8
hiei2716 <s-k.exvs.only@ezweb.ne.jp>
2020-06-10 09:53:23 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  root 'items#show'
+  root 'items#index'
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index] do
+    collection do
+      get 'logout'
+    end
+  end
+  resources :credit_cards, only: [:new]
+  resources :purchase, only: [:index]
+  resources :items, only: [:index, :new, :show]
 end
