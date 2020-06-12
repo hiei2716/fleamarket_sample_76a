b517dc54a726ab5fba50e99270563ae3a21a88d7
RyunosukeHorihata <ryunosuke.horihata@gmail.com>
2020-06-12 18:26:47 +0900

    ルーティング設定：商品情報編集ページへの遷移のため

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   end
   resources :credit_cards, only: [:new]
   resources :purchase, only: [:index]
+  get 'items/edit', to: 'items#edit' #商品情報編集ページへの遷移のため
   resources :items, only: [:index, :new, :show, :edit, :update]
 end
