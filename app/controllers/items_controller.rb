f7fbe9d647f9bca919e6f3169e8f1a438b41aee8
RyunosukeHorihata <ryunosuke.horihata@gmail.com>
2020-06-12 17:56:59 +0900

    itemsコントローラーのedit及びupdateアクションを仮定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,9 +8,12 @@ class ItemsController < ApplicationController
   def new
   end
 
-  def edit
+  def edit  #事前に商品情報編集用アクションを定義
+    # @item = Item.find(params[:id])  
   end
 
-  def update
+  def update  #事前に商品情報更新用アクションを定義
+    # item = Item.find(params[:id])
+    # item.update(item_params)
   end
 end
