5d2abebe79175204c3844c3b0698399786bb20ae
Yu-41 <hirosi41@gmail.com>
2020-06-12 11:30:36 +0900

    複数画像投稿機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,9 @@ class Item < ApplicationRecord
   # has_many :comments
   belongs_to :category
 
+  has_many :images, dependent: :destroy
+  accepts_nested_attributes_for :images, allow_destroy: true
+
   belongs_to_active_hash :prefecture
   # belongs_to_active_hash :condition
   belongs_to_active_hash :brand
