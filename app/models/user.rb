040533c345613b5c03c9f180c6ecdb9c3a9ebea1
shin-nkt-09 <shin.nkt.09@gmail.com>
2020-06-14 16:30:18 +0900

    ユーザ新規登録サーバサイド完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,13 @@ class User < ApplicationRecord
     has_many :items
 
   # バリデーション
-  validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_date, :phone_number, :gender, :password_confirmation,  presence: true
+  validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_date, :phone_number, :gender, presence: true
 
   # 文字の形式制限
   validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/,
     message: "全角文字のみが使えます" }
   validates :family_name_kana,:first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/,
     message: "全角カナのみが使えます" }
+
 
   end
