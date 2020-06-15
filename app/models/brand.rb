7db7688d10ab950e2c5cbf7fc8931f8650034378
hiei2716 <s-k.exvs.only@ezweb.ne.jp>
2020-06-15 21:56:58 +0900

    微調整(1)

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,8 @@
-class Brand < ApplicationRecord
-  has_many :items
+class Brand < ActiveHash::Base
+  self.data = [
+    {id: 1, name: 'ノーブランド'}, {id: 2, name: 'プーマ'}, {id: 3, name: 'ナイキ'},
+    {id: 4, name: 'アディダス'}, {id: 5, name: 'コンバース'}, {id: 6, name: 'ニューバランス'},
+    {id: 7, name: 'HITACHI'}, {id: 8, name: '三菱'}, {id: 9, name: 'SONY'},
+    {id: 10, name: '富士通'}, {id: 11, name: 'NEC'}, {id: 12, name: 'Panasonic'}
+  ]
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,18 @@
 class Item < ApplicationRecord
-  belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  validates :user_id, :name, :description, :price, :condition_id, :wait, :postage, :category_id, :prefecture_id, presence: true
+  validates :images, presence: true
+  validates :price, :numericality => { :greater_than => 299 }
+  validates :price, :numericality => { :less_than => 9999999 }
+  
+  belongs_to :user, optional: true
   has_many :comments
-  has_many :images
-  belongs_to :brand
   belongs_to :category
+
   has_many :images, dependent: :destroy
-  # belongs_to_active_hash :prefecture
-  # belongs_to_active_hash :category
-  # belongs_to_active_hash :brand
+  accepts_nested_attributes_for :images, allow_destroy: true
+
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :brand
 end
