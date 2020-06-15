624a71b761604b9ed89b59989a53b2fb8d33d9c6
Yu-41 <hirosi41@gmail.com>
2020-06-15 11:18:13 +0900

    出品機能に伴うリレーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,13 +6,13 @@ class Item < ApplicationRecord
   validates :price, :numericality => { :less_than => 9999999 }
   
   belongs_to :user, optional: true
-  # has_many :comments
+  has_many :comments
   belongs_to :category
 
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
 
   belongs_to_active_hash :prefecture
-  # belongs_to_active_hash :condition
+  belongs_to_active_hash :condition
   belongs_to_active_hash :brand
 end
