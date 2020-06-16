6cbb447837243a22595a99fc8a6b25b2d1e86cd3
shin-nkt-09 <shin.nkt.09@gmail.com>
2020-06-16 17:49:36 +0900

    マイグレーションファイル修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_032405) do
+ActiveRecord::Schema.define(version: 2020_06_10_033405) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "family_name", null: false
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2020_06_10_032405) do
     t.string "local", null: false
     t.string "block", null: false
     t.string "building"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
