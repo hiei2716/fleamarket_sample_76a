414ad81c1a8fe0b0f396620c6858a0be46de6636
TOSHIYUKIHATA <hata.toshiyuki09@gmail.com>
2020-06-16 16:34:19 +0900

    マイグレーションファイル名変更
    マイグレーションファイル名変更

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 2020_06_10_054138) do
     t.string "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["ancestry"], name: "index_category_ancestries_on_ancestry", using: :btree
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
