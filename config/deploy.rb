cc9477316b60dd484c8b181e5c3cf4bac0e42308
hiei2716 <s-k.exvs.only@ezweb.ne.jp>
2020-06-19 11:36:08 +0900

    微調整(88)

--- config/deploy.rb
+++ config/deploy.rb
@@ -70,3 +70,12 @@ namespace :deploy do
   before :starting, 'deploy:upload'
   after :finishing, 'deploy:cleanup'
 end
+task :db_seed do
+  on roles(:db) do |host|
+    with rails_env: fetch(:rails_env) do
+      within current_path do
+        execute :bundle, :exec, :rake, 'db:seed'
+      end
+    end
+  end
+end
