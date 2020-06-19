75ded1f4bac6effd7eb836739f59ca721dfe576d
shin-nkt-09 <shin.nkt.09@gmail.com>
2020-06-19 18:13:56 +0900

    brandseed初期データ導入

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_06_10_033405) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
+    t.string "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
